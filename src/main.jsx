42e09b9ae7f05f10eef0e05a6c912ef773efac7f
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-09-11 20:55:48 +0400

    feature/isOn

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import App2 from "./App2.jsx";
 
-createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<App2 />);
